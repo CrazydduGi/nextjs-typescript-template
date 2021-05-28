8267fada1b92b075d3bf4e241398ac6853b7d48d
Jungjae Kim <jungjaekim@gmail.com>
2021-05-29 01:19:46 +0900

    Adapt gitignore

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
-export default MyApp
+import type { AppProps } from 'next/app';
+import { FC } from 'react';
+
+// eslint-disable-next-line react/jsx-props-no-spreading
+const MyApp: FC<AppProps> = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />;
+export default MyApp;
